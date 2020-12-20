ff95cd7890d21d8df001fc967c0333ff0d9c7ede
Navin Sivakumar <navin.sivakumar@gmail.com>
2020-12-20 17:32:12 -0500

    Cleanup: Reverse ordering to be in forward dependency order, since my brain prefers that

--- main.tf
+++ main.tf
@@ -3,8 +3,45 @@ provider "google" {
   region  = var.region
 }
 
-output "spoke_url" {
-  value = google_cloud_run_service.spoke-server.status[0].url
+# Generate a random suffix for SQL instances, since there is a delay before you
+# are allowed to reuse instance names.
+resource "random_id" "sql" {
+  prefix = "spoke-sql"
+  byte_length = "8"
+}
+
+resource "google_sql_database_instance" "spoke-db" {
+  name   = random_id.sql.hex
+  region = var.region
+  settings {
+    tier = "db-f1-micro"
+
+    ip_configuration {
+      ipv4_enabled = true
+    }
+
+    backup_configuration {
+      enabled = false
+    }
+  }
+
+  database_version = "POSTGRES_12"
+
+  # Allow deletion during development.
+  # TODO: Make this configurable so production instances cannot be deleted by
+  # Terraform.
+  deletion_protection  = "false"
+}
+
+resource "random_password" "sql-password" {
+  length = 16
+}
+
+resource "google_sql_user" "spoke-db-user" {
+  name = "sqluser"
+  instance = google_sql_database_instance.spoke-db.name
+  password = random_password.sql-password.result
+  deletion_policy = "ABANDON"
 }
 
 resource "google_cloud_run_service" "spoke-server" {
@@ -64,43 +101,6 @@ resource "google_cloud_run_service" "spoke-server" {
   autogenerate_revision_name = true
 }
 
-resource "google_sql_user" "spoke-db-user" {
-  name = "sqluser"
-  instance = google_sql_database_instance.spoke-db.name
-  password = random_password.sql-password.result
-  deletion_policy = "ABANDON"
-}
-
-resource "random_password" "sql-password" {
-  length = 16
-}
-
-resource "google_sql_database_instance" "spoke-db" {
-  name   = random_id.sql.hex
-  region = var.region
-  settings {
-    tier = "db-f1-micro"
-
-    ip_configuration {
-      ipv4_enabled = true
-    }
-
-    backup_configuration {
-      enabled = false
-    }
-  }
-
-  database_version = "POSTGRES_12"
-
-  # Allow deletion during development.
-  # TODO: Make this configurable so production instances cannot be deleted by
-  # Terraform.
-  deletion_protection  = "false"
-}
-
-# Generate a random suffix for SQL instances, since there is a delay before you
-# are allowed to reuse instance names.
-resource "random_id" "sql" {
-  prefix = "spoke-sql"
-  byte_length = "8"
+output "spoke_url" {
+  value = google_cloud_run_service.spoke-server.status[0].url
 }
