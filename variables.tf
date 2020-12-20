e3b0f687af4ae4009d710b3d51f2c095709ee8a1
Navin Sivakumar <navin.sivakumar@gmail.com>
2020-12-19 19:51:48 -0500

    Set up google tf provider

---
+++ variables.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 0.14"
+}
+
+variable "region" {
+  type        = string
+  default     = "us-central1"
+  description = "Region in which to run Spoke."
+}
+
+variable "project" {
+  type        = string
+  default     = ""
+  description = "Project in which Terraform will create resources."
+}
